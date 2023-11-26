839588217c366df8bb274ac9efd1f7d417ac7e93
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-11-26 19:38:47 -0300

    Update teste_bombeamento.py

--- Projeto_Final/teste_bombeamento.py
+++ Projeto_Final/teste_bombeamento.py
@@ -1,87 +1,61 @@
 import tkinter as tk
-from tkinter import filedialog
 
+def lema_bombeamento(palavra):
+ # Obtém o comprimento da palavra.
+ k=0
+ comprimento = len(palavra)
 
-def verificar_igualdade_substr(s, p, q, r):
-    if p == q or q == r:
-        return False
-    return s[p:q] == s[q:r]
+ # Define o valor de p como o menor divisor de comprimento que é maior ou igual a 2.
+ i = int(input_i.get())
+ # Obtém as sequências u, v e z.
+ u = palavra[0]
+ v = palavra[1]
+ z = palavra[2:] 
 
-def lema_bombeamento(linguagem, n):
-    for s in linguagem:
-        if len(s) >= n:
-            for i in range(len(s) - n + 1):
-                for j in range(i + 1, len(s) + 1):
-                    if j - i < n:
-                        continue
-                    if verificar_igualdade_substr(s, i, i + n, j):
-                        return False
-    return True
+ # Verifica se a palavra montada é igual à palavra formada.
+ palavra_montada = u + v + z
+ palavra_formada = ""
+ palavra_formada += u
+ for k in range(0, i):
+  palavra_formada += v
+  i-=1
+ palavra_formada += z
 
-def verificar_linguagens():
-    nome_arquivo = entry_nome_arquivo.get()
-    n = int(entry_n.get())
-    
-    try:
-        with open(nome_arquivo, 'r') as arquivo:
-            linguagens = arquivo.readlines()
+ if palavra_montada == palavra_formada:
+  return True
+ else:
+  return False
 
-        linguagens = [linha.strip() for linha in linguagens]
+def main():
+ # Obtém a palavra digitada pelo usuário.
+ palavra = input_palavra.get()
 
-        todas_regulares = True
+ # Aplica o lema do bombeamento.
+ if lema_bombeamento(palavra):
+  result_label.config(text="A LINGUAGEM PODE SER REGULAR")
+ else:
+  result_label.config(text="A LINGUAGEM NAO E REGULAR")
 
-        result_text.delete(1.0, tk.END)
-
-        for i, linguagem in enumerate(linguagens):
-            if lema_bombeamento([linguagem], n):
-                result_text.insert(tk.END, f"Linguagem {i + 1}: '{linguagem}' pode ser regular.\n")
-            else:
-                result_text.insert(tk.END, f"Linguagem {i + 1}: '{linguagem}' não é regular.\n")
-                todas_regulares = False
-
-        if todas_regulares:
-            result_text.insert(tk.END, "Todas as linguagens no arquivo são regulares.")
-        else:
-            result_text.insert(tk.END, "Pelo menos uma das linguagens no arquivo pode não ser regular.")
-
-    except FileNotFoundError:
-        result_text.insert(tk.END, f"Arquivo '{nome_arquivo}' não encontrado.")
-    except Exception as e:
-        result_text.insert(tk.END, f"Ocorreu um erro: {str(e)}")
-
-def selecionar_arquivo():
-    nome_arquivo = filedialog.askopenfilename()
-    entry_nome_arquivo.delete(0, tk.END)
-    entry_nome_arquivo.insert(0, nome_arquivo)
-
-# Cria a janela principal
 root = tk.Tk()
-root.title("Verificação de Linguagens Regulares")
+root.title("Lema do Bombeamento")
+root.geometry('300x150') # Define o tamanho inicial da janela
 
-# Rótulo e entrada para o nome do arquivo
-label_nome_arquivo = tk.Label(root, text="Nome do Arquivo:")
-label_nome_arquivo.pack()
-entry_nome_arquivo = tk.Entry(root)
-entry_nome_arquivo.pack()
+palavra_label = tk.Label(root, text="Palavra: ")
+palavra_label.pack()
 
-# Botão para selecionar o arquivo
-button_selecionar = tk.Button(root, text="Selecionar Arquivo", command=selecionar_arquivo)
-button_selecionar.pack()
+input_palavra = tk.Entry(root)
+input_palavra.pack()
 
-# Rótulo e entrada para o valor de n
-label_n = tk.Label(root, text="Valor de n:")
-label_n.pack()
-entry_n = tk.Entry(root)
-entry_n.pack()
+i_label = tk.Label(root, text="Valor de i: ")
+i_label.pack()
 
-# Botão para verificar as linguagens
-button_verificar = tk.Button(root, text="Verificar Linguagens", command=verificar_linguagens)
-button_verificar.pack()
+input_i = tk.Entry(root)
+input_i.pack()
 
-# Área de texto para exibir os resultados
-result_text = tk.Text(root, height=10, width=50)
-result_text.pack()
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+button = tk.Button(root, text="Verificar", command=main)
+button.pack()
 
 root.mainloop()
-
-#verificar_linguagens_em_arquivo(nome_arquivo, n)
