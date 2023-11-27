142881408b62492ddca889e0d7ad44aaace83315
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-11-27 15:58:31 -0300

    Update teste_bombeamento.py

--- Projeto_Final/teste_bombeamento.py
+++ Projeto_Final/teste_bombeamento.py
@@ -1,18 +1,45 @@
 import tkinter as tk
+import itertools
+
+def gerar_palavras(n):
+   # Cria uma lista vazia para armazenar as palavras
+   lista_palavras = []
+
+   # aux = 1 - l1
+   if aux == 1:
+   # Adiciona 'a' e 'b' à palavra n vezes
+    for i in range(n):
+         lista_palavras.append('a' * (i + 1) + 'b' * (i + 1))
+   #aux = 2 - l2
+   if aux == 2:
+    for i in range(100):
+     for j in range(n):
+        lista_palavras.append('a' * (i + 1) + 'b' * (j + 1))
+   return lista_palavras
 
 def lema_bombeamento(palavra):
  # Obtém o comprimento da palavra.
- k=0
  comprimento = len(palavra)
+ k=0
+  # Verifica se todos os caracteres pertencem ao alfabeto {a, b}.
+ if not all(c in 'ab' for c in palavra):
+  raise ValueError("Caracter nao pertence ao alfabeto")
+
+ lista_palavras = gerar_palavras(comprimento)
+
+ #for p in lista_palavras:
+   #print(p)
 
  # Define o valor de p como o menor divisor de comprimento que é maior ou igual a 2.
+
+ #pega o valor de i do usuario 
  i = int(input_i.get())
  # Obtém as sequências u, v e z.
  u = palavra[0]
  v = palavra[1]
  z = palavra[2:] 
 
- # Verifica se a palavra montada é igual à palavra formada.
+ # Verifica se a palavra montada pertence a linguagem
  palavra_montada = u + v + z
  palavra_formada = ""
  palavra_formada += u
@@ -21,7 +48,7 @@ def lema_bombeamento(palavra):
   i-=1
  palavra_formada += z
 
- if palavra_montada == palavra_formada:
+ if palavra_formada in lista_palavras:
   return True
  else:
   return False
@@ -40,6 +67,26 @@ root = tk.Tk()
 root.title("Lema do Bombeamento")
 root.geometry('300x150') # Define o tamanho inicial da janela
 
+# Cria um menu de opções para escolher a linguagem
+def escolha_linguagem(*args):
+  global linguagem
+  linguagem = linguagem_var.get()
+   # Atribui o valor de aux com base na escolha do usuário
+  global aux
+  if linguagem == "L={ a^n b^n | n ≥ 0}":
+     aux = 1
+  elif linguagem == "L={ a^m b^n | n ≥ 0 e m ≥ 0}":
+     aux = 2
+
+# Cria uma variável para armazenar a linguagem selecionada
+linguagem_var = tk.StringVar(root)
+linguagem_var.set("L={ a^n b^n | n ≥ 0}") # valor padrão
+linguagem_var.trace("w", escolha_linguagem)
+
+# Adiciona o menu de opções à janela
+optionmenu = tk.OptionMenu(root, linguagem_var, "L={ a^n b^n | n ≥ 0}", "L={ a^m b^n | n ≥ 0 e m ≥ 0}")
+optionmenu.pack()
+
 palavra_label = tk.Label(root, text="Palavra: ")
 palavra_label.pack()
 
@@ -57,5 +104,6 @@ result_label.pack()
 
 button = tk.Button(root, text="Verificar", command=main)
 button.pack()
+
 
 root.mainloop()
